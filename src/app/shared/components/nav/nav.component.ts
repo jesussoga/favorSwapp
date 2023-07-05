cfa003968902eec36f283008c40f8232c1b799e1
loly <loly@gmail.com>
2023-07-05 13:47:10 +0200

    Hecho el nav y logo, pero faltan las rutas una vez aclarado todo

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, PrimeIcons} from "primeng/api";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent{
+
+  //   public items: MenuItem[];
+  //   public activeItem: MenuItem;
+  //
+  // constructor() {
+  //   this.items = [];
+  //   this.activeItem = {};
+  // }
+  //
+  //
+  //
+  //
+  //
+  // ngOnInit() {
+  //   this.items = [
+  //     {label: 'Inicio', icon: PrimeIcons.HOME},
+  //     {label: 'Publicar Favor', icon: PrimeIcons.SEND},
+  //     {label: 'Buscar Favor', icon: PrimeIcons.SEARCH},
+  //     {label: 'Login', icon: PrimeIcons.USER}
+  //   ];
+  // }
 
 }
