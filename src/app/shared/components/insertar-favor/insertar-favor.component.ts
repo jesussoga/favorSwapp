9b5371b27dd4ce97f29adc0cfbe39d74bfbfc0d0
jesussoga <em.sotoca@gmail.com>
2023-07-10 14:19:29 +0200

    edición en el archivo favor-swapp.service.ts, filtros.component.html, filtros.component.ts, insertar-favor.component.html y insertar-favor.component.ts

--- src/app/shared/components/insertar-favor/insertar-favor.component.ts
+++ src/app/shared/components/insertar-favor/insertar-favor.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, NgForm, Validators} from "@angular/forms";
+import {Favor} from "../../models/favor.models";
+import {FavorSwappService} from "../../services/favor-swapp.service";
 
 @Component({
   selector: 'app-insertar-favor',
@@ -11,7 +13,10 @@ export class InsertarFavorComponent implements OnInit{
   public pruebasDesarrolo: boolean;
   public formFavor!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private favorService: FavorSwappService,
+  ) {
     this.pruebasDesarrolo = true;
   }
   ngOnInit(): void {
@@ -20,15 +25,28 @@ export class InsertarFavorComponent implements OnInit{
 
   public obtenerFormulario(){
     this.formFavor = this.formBuilder.group({
-      foto           : ["", [Validators.required]],
-      fumar             : [],
-      internet          : [],
-      mascota           : [],
-      climatizacion     : [],
-      adapMovRedu      : []
+      id                : [0, []],
+      foto              : ["", [Validators.required]],
+      fumar             : [false, []],
+      internet          : [false, []],
+      mascota           : [false, []],
+      climatizacion     : [false, []],
+      adaptacionMovilidadReducida      : [false, []]
     });
 
   }
 
+  public validarFavor(){
+    let favor: Favor = this.formFavor.value;
+    this.guardaFavor(favor);
+  }
 
+  public guardaFavor(favor: Favor) {
+    // TODO
+    console.log(favor);
+    this.favorService.guardarFavor(favor).subscribe({
+      next : () => {},
+      error : () => {}
+    });
+  }
 }
