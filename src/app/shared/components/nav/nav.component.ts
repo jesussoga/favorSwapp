c7cb8120ca5881a82eba9663a5e4261238e55a02
Java <fjvalls@gmail.com>
2023-07-15 18:34:08 +0200

    Modificado nav.component.ts para que get usuarioActivo() devuelva un usuario, en vez de el servicio. Modificado nav.component.html para utilizar correctamente esta modificación.

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {MenuItem, PrimeIcons} from "primeng/api";
+import {MenuItem,} from "primeng/api";
 import {AuthService} from "../../../auth/services/auth.service";
 import {Router} from "@angular/router";
 import {Usuario} from "../../models/favor.models";
